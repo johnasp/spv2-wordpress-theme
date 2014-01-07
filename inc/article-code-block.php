94fe7cd86316bd03cb47181472a5a304cbac0a8e
John Aspinall <john.aspinall73@gmail.com>
2014-01-07 14:56:37 +0000

    Add thumbnail to homepage sub stories

--- inc/article-code-block.php
+++ inc/article-code-block.php
@@ -2,8 +2,9 @@
     <div class="post-meta" aria-hidden="true"><?php the_date(); ?><span data-icon="G"><?php the_category(', '); ?> </span></div>
     <header> 
       <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-        <?php the_excerpt(); ?>  
         <?php the_post_thumbnail();?> 
+        <?php the_excerpt(); ?>  
+        
       <a class="more" href="<?php the_permalink(); ?>" data-icon="9">Listen</a>
     </header>   
 </article>   
