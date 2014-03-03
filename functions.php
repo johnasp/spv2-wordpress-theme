c5eac382d9a8a4c9e81036e637d9f9b0eb82d610
John Aspinall <john.aspinall73@gmail.com>
2014-03-03 16:27:31 +0000

    Image added to RSS feed

--- functions.php
+++ functions.php
@@ -111,8 +111,17 @@ function the_category_unlinked($separator = ' ') {
     echo $thelist;
 }
 
+//ADDS FEATURED IMAGE TO RSS FEED
 
-
+function insertThumbnailRSS($content) {
+global $post;
+if ( has_post_thumbnail( $post->ID ) ){
+$content = '' . get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'alt' => get_the_title(), 'title' => get_the_title(), 'style' => 'float:right;' ) ) . '' . $content;
+}
+return $content;
+}
+add_filter('the_excerpt_rss', 'insertThumbnailRSS');
+add_filter('the_content_feed', 'insertThumbnailRSS');
 
 
 ?>
