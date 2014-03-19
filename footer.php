db15f3dbc3ad57078c6bb92942d72e0e9b71f63f
John Aspinall <john.aspinall73@gmail.com>
2014-03-19 14:20:44 +0000

    Related posts moved to body

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="grid-1 group">
-<div class="module">Copyright &copy; <a href="https://plus.google.com/+JohnAspinall73" rel="me">John Aspinall</a>.  Follow the pod on <a href="https://plus.google.com/+SeasiderspodcastCoUk/" rel="publisher">Google+</a></div>
+<div class="module">This site was lovingly hand coded and converted into a Wordpress theme by <a href="https://plus.google.com/+JohnAspinall73" rel="me">John Aspinall</a>. G+ the pod on <a href="https://plus.google.com/+SeasiderspodcastCoUk/" rel="publisher">Google+</a></div>
 </footer>
     
 </div><!-- grid -->    
--- single.php
+++ single.php
@@ -49,6 +49,41 @@
             </div>
           </nav>-->
 
+          <section id="related-posts">
+
+              <h3>Related posts</h3>
+              <?php
+              //for use in the loop, list 6 post titles related to first tag on current post
+              $tags = wp_get_post_tags($post->ID);
+              if ($tags) {
+                $first_tag = $tags[0]->term_id;
+                $args=array(
+                'tag__in' => array($first_tag),
+                'post__not_in' => array($post->ID),
+                'posts_per_page'=>6,
+                'caller_get_posts'=>1
+              );
+              $my_query = new WP_Query($args);
+              if( $my_query->have_posts() ) {
+              while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+
+              <figure>
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+                <?php the_post_thumbnail('thumbnail'); ?>
+                <figcaption><span aria-hidden="true" data-icon="8"></span><?php the_title(); ?></figcaption>
+              </a>
+              </figure>
+
+              <?php
+              endwhile;
+              }
+              wp_reset_query();
+              }
+              ?>
+
+          </section>
+
           <?php 
            if  ($newPod == "true") { 
               include(TEMPLATEPATH."/inc/podcast-only.php");
@@ -58,12 +93,7 @@
           
       </article>
 
-<section id="related-posts" class="module archives">
 
-  <h2>Related Posts</h2>
-  <?php include(TEMPLATEPATH."/inc/related-posts.php");?>
-
-</section>
 
     <?php
     } // end while
