949d869067adeb2501dffab2daebb8d9992fbf5e
John <john.aspinall73@gmail.com>
2014-09-29 11:25:23 +0100

    Last-match.php force deploy

--- inc/last-match.php
+++ inc/last-match.php
@@ -8,7 +8,7 @@
         echo '<h4>Team:</h4>' . $row['team'];
         echo '<h4>Match highlights:</h4>' . $row['highlights'];
         echo '<h4>Fans comments:</h4>' . $row['fans_comments'];
-        echo '<h4>Manager audio:</h4>' . $row['manager_audio'];
+        echo '<h4>Manager audio:</h4>' . $row['manager_audio'];  
       }
     }  
 ?>
