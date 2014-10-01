f18cbe95a51c624e6fa336503fbe660179f997e1
John <john.aspinall73@gmail.com>
2014-10-01 09:41:10 +0100

    Last match review hiding
    Wrote conditional logic to hide last match review content blocks from
    displaying on the page if they don’t have any data in them.

--- inc/last-match.php
+++ inc/last-match.php
@@ -4,11 +4,35 @@
     if($rows) {
       foreach($rows as $row)
       {
-        echo '<h3>' . $row['result'] . '</h3>';
-        echo '<h4>Team:</h4>' . $row['team'];
-        echo '<h4>Match highlights:</h4>' . $row['highlights'];
-        echo '<h4>Fans comments:</h4>' . $row['fans_comments'];
+        if (empty($row['result'])) {
+          
+        } 
+        else {
+          echo '<h3>' . $row['result'] . '</h3>';
+        }
+        if (empty($row['team'])) {      
+        } 
+        else {
+         echo '<h4>Team:</h4>' . $row['team'];
+        }
+        if (empty($row['highlights'])) {
+          
+        } 
+        else {
+          echo '<h4>Match highlights:</h4>' . $row['highlights'];
+        }
+        if (empty($row['fans_comments'])) {
+          
+        } 
+        else {
+          echo '<h4>Fans comments:</h4>' . $row['fans_comments'];
+        }
+        if (empty($row['manager_audio'])) {
+          
+        } 
+        else {
         echo '<h4>Manager audio:</h4>' . $row['manager_audio'];  
+        }
       }
     }  
 ?>
