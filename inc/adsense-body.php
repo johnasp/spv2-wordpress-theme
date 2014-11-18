d986c1be09aaeae5a7dbf24ee0980a47578cb436
John <john.aspinall73@gmail.com>
2014-11-18 12:20:36 +0000

    Responsive adsense
    Changed the adsense unit to be responsive

--- inc/adsense-body.php
+++ inc/adsense-body.php
@@ -1,8 +1,10 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Responsive ad -->
 <ins class="adsbygoogle"
-   style="display:inline-block;width:468px;height:60px"
-   data-ad-client="ca-pub-0301430055096164"
-   data-ad-slot="9710668322"></ins>
+     style="display:block"
+     data-ad-client="ca-pub-0301430055096164"
+     data-ad-slot="3025696016"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
